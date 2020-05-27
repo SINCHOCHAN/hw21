96b6e4c9c3f3bffd80d80edf7449b568b4866ad0
Terbee <terencechan@berkeley.edu>
2020-05-27 14:04:59 -0700

    1

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure body parsing for AJAX requests
+// define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+// Connect to heroku 
+mongoose.connect(
+  process.env.MONGODB_URI  || 'mongodb://localhost/googlebooks', 
+
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
